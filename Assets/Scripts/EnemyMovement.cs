5e6c977442aa230778a3b798b2d2a7397491e453
DevThompson <devaughnwthompson@hotmail.com>
2023-05-10 20:13:14 -0400

    10-MAY-23 Changes
    Enemies now turn around if they hit a ledge
    Cleaned up enemy movement code
    Cleaned up enemy1 colliders

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -26,29 +26,38 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      //Checks if enemy is attacking
-      enemyAttacking = enemyAnimator.GetBool("isAttacking");
+      Movement();
+      AttackCheck(); 
+    }
 
+    void Movement()
+    {
 
       //if enemy isn't attacking move enermy
       if(enemyAttacking == false)
       {
         myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+        transform.localScale = new Vector3 ((Mathf.Sign(myRigidbody.velocity.x)), 1f);
         enemyAnimator.SetBool("isMoving", true);
       }    
-      
       //if front of enemy touches wall or the ground turn around
       if(turnCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Wall")) || turnCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
       {
         moveSpeed = -moveSpeed;
-        FlipEnemyFacing();
       }
-        
     }
 
-    //Flips direction enemy is facing
-    void FlipEnemyFacing()
+    void AttackCheck()
     {
-      transform.localScale = new Vector3 (-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
+      //Checks if enemy is attacking
+      enemyAttacking = enemyAnimator.GetBool("isAttacking");
+    }
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+      if (other.tag =="Ground")
+      {
+        moveSpeed = -moveSpeed;
+      } 
     }
 }
